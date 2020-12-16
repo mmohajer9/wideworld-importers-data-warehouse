0999a72168977c76b9ea0ea8a2728404aa457232
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-12-16 23:55:10 +0330

    Staging for warehouse completed

--- Source/Staging/Warehouse/StagingOperations.sql
+++ Source/Staging/Warehouse/StagingOperations.sql
@@ -1,23 +1,203 @@
--- Create a new stored procedure called 'FillStagingStockItems' in schema 'dbo'
--- Drop the stored procedure if it already exists
-IF EXISTS (
-SELECT *
-    FROM INFORMATION_SCHEMA.ROUTINES
-WHERE SPECIFIC_SCHEMA = N'dbo'
-    AND SPECIFIC_NAME = N'FillStagingStockItems'
-)
-DROP PROCEDURE dbo.FillStagingStockItems
-GO
--- Create the stored procedure in the specified schema
-CREATE PROCEDURE dbo.FillStagingStockItems
-    -- @param1 /*parameter name*/ int /*datatype_for_param1*/ = 0, /*default_value_for_param1*/
-    -- @param2 /*parameter name*/ int /*datatype_for_param1*/ = 0 /*default_value_for_param2*/
--- add more stored procedure parameters here
+CREATE OR ALTER PROCEDURE FillStagingStockItems
 AS
-    -- body of the stored procedure
-    
+BEGIN
+    TRUNCATE TABLE StagingStockItems
+    INSERT INTO StagingStockItems
+        (
+        [StockItemID],
+        [StockItemName],
+        [SupplierID],
+        [ColorID],
+        [UnitPackageID],
+        [OuterPackageID],
+        [Brand],
+        [Size],
+        [IsChillerStock],
+        [Barcode],
+        [TaxRate],
+        [UnitPrice],
+        [RecommendedRetailPrice],
+        [TypicalWeightPerUnit]
+        )
+    SELECT
+        [StockItemID],
+        [StockItemName],
+        [SupplierID],
+        [ColorID],
+        [UnitPackageID],
+        [OuterPackageID],
+        [Brand],
+        [Size],
+        [IsChillerStock],
+        [Barcode],
+        [TaxRate],
+        [UnitPrice],
+        [RecommendedRetailPrice],
+        [TypicalWeightPerUnit]
+    FROM [WideWorldImporters].[Warehouse].[StockItems]
+
+END
+--------------------------------------------------------------------------------------------------------------
 GO
 
-EXECUTE dbo.FillStagingStockItems;
 
+CREATE OR ALTER PROCEDURE FillStagingStockItemHoldings
+AS
+BEGIN
+    TRUNCATE TABLE StagingStockItemHoldings
+    INSERT INTO StagingStockItemHoldings
+        (
+        [StockItemID],
+        [QuantityOnHand],
+        [BinLocation],
+        [LastStocktakeQuantity],
+        [LastCostPrice],
+        [ReorderLevel],
+        [TargetStockLevel]
+        )
+    SELECT
+        [StockItemID],
+        [QuantityOnHand],
+        [BinLocation],
+        [LastStocktakeQuantity],
+        [LastCostPrice],
+        [ReorderLevel],
+        [TargetStockLevel]
+
+    FROM [WideWorldImporters].[Warehouse].[StockItemHoldings]
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+
+CREATE OR ALTER PROCEDURE FillStagingStockGroups
+AS
+BEGIN
+    TRUNCATE TABLE StagingStockGroups
+    INSERT INTO StagingStockGroups
+        (
+        [StockGroupID],
+        [StockGroupName]
+        )
+    SELECT
+        [StockGroupID],
+        [StockGroupName]
+
+    FROM [WideWorldImporters].[Warehouse].[StockGroups]
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+CREATE OR ALTER PROCEDURE FillStagingStockItemStockGroups
+AS
+BEGIN
+    TRUNCATE TABLE StagingStockItemStockGroups
+    INSERT INTO StagingStockItemStockGroups
+        (
+        [StockGroupID],
+        [StockGroupName]
+        )
+    SELECT
+        [StockGroupID],
+        [StockGroupName]
+
+    FROM [WideWorldImporters].[Warehouse].[StockItemStockGroups]
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+
+CREATE OR ALTER PROCEDURE FillStagingPackageTypes
+AS
+BEGIN
+    TRUNCATE TABLE StagingPackageTypes
+    INSERT INTO StagingPackageTypes
+        (
+        [PackageTypeID],
+        [PackageTypeName]
+        )
+    SELECT
+        [PackageTypeID],
+        [PackageTypeName]
+
+    FROM [WideWorldImporters].[Warehouse].[PackageTypes]
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+CREATE OR ALTER PROCEDURE FillStagingColors
+AS
+BEGIN
+    TRUNCATE TABLE StagingColors
+    INSERT INTO StagingColors
+        (
+        [ColorID],
+        [ColorName]
+        )
+    SELECT
+        [ColorID],
+        [ColorName]
+
+    FROM [WideWorldImporters].[Warehouse].[Colors]
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+CREATE OR ALTER PROCEDURE FillStagingTransactionTypes
+AS
+BEGIN
+    TRUNCATE TABLE StagingTransactionTypes
+    INSERT INTO StagingTransactionTypes
+        (
+        [TransactionTypeID],
+        [TransactionTypeName]
+        )
+    SELECT
+        [TransactionTypeID],
+        [TransactionTypeName]
+
+    FROM [WideWorldImporters].[Warehouse].[TransactionTypes]
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+CREATE OR ALTER PROCEDURE FillStagingStockItemTransactions
+AS
+BEGIN
+    DECLARE @last_added DATE = (
+        SELECT isnull(max([TransactionDate]),'2012-12-31')
+        FROM [WideWorldImporters].[Warehouse].[StockItemTransactions]
+    )
+    INSERT INTO StagingCustomerTransactions
+    (
+        [StockItemTransactionID],
+        [StockItemID],
+        [TransactionTypeID],
+        [CustomerID],
+        [InvoiceID],
+        [SupplierID],
+        [PurchaseOrderID],
+        [TransactionOccurredWhen],
+        [Quantity]
+    )
+    SELECT
+        [StockItemTransactionID],
+        [StockItemID],
+        [TransactionTypeID],
+        [CustomerID],
+        [InvoiceID],
+        [SupplierID],
+        [PurchaseOrderID],
+        [TransactionOccurredWhen],
+        [Quantity]
+    FROM [WideWorldImporters].[Warehouse].[StockItemTransactions]
+    WHERE TransactionOccurredWhen > @last_added
+END
+--------------------------------------------------------------------------------------------------------------
 GO
