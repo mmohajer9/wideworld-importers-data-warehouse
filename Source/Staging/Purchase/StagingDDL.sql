0709d9df4e7cc762b8dfc2ae5c6cb248ff1846d5
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-12-21 00:19:08 +0330

    staging area for purchase is fulfilled

--- Source/Staging/Purchase/StagingDDL.sql
+++ Source/Staging/Purchase/StagingDDL.sql
@@ -1,56 +1,84 @@
 IF OBJECT_ID('dbo.StagingPurchaseOrder', 'U') IS NOT NULL
-    drop table StagingPurchaseOrder
-CREATE TABLE StagingPurchaseOrder(
-	PurchaseOrderID int NOT NULL PRIMARY KEY,
-	SupplierID int NOT NULL,
-	OrderDate date NOT NULL,
-	DeliveryMethodID int NOT NULL,
-	ContactPersonID int NOT NULL,
-	ExpectedDeliveryDate date NULL,
-	SupplierReference nvarchar(20) NULL,
-	IsOrderFinalized bit NOT NULL,
-	Comments nvarchar(max) NULL,
-	InternalComments nvarchar(max) NULL,
-	LastEditedBy int NOT NULL,
-	LastEditedWhen datetime2(7) NOT NULL,
+    DROP TABLE StagingPurchaseOrder
+CREATE TABLE StagingPurchaseOrder
+(
+	PurchaseOrderID INT NOT NULL PRIMARY KEY,
+	SupplierID INT NOT NULL,
+	OrderDate DATE NOT NULL,
+	DeliveryMethodID INT NOT NULL,
+	ContactPersonID INT NOT NULL,
+	ExpectedDeliveryDate DATE NULL,
+	SupplierReference NVARCHAR(50) NULL,
+	IsOrderFinalized NVARCHAR(10) NOT NULL,
+	--^ bit in original source --> 1 : yes , 0 : no
 );
 GO
 
+IF OBJECT_ID('dbo.StagingSupplierCategories' , 'U') IS NOT NULL
+	DROP TABLE StagingSupplierCategories
 
-IF OBJECT_ID('dbo.StagingSuplier', 'U') IS NOT NULL
-    drop table StagingSuplier
-CREATE TABLE StagingSuplier(
-	SupplierID int NOT NULL PRIMARY KEY,
-	SupplierName nvarchar(100) NOT NULL,
-	SupplierCategoryID int NOT NULL,
-	PrimaryContactPersonID int NOT NULL,
-	AlternateContactPersonID int NOT NULL,
-	DeliveryMethodID int NULL,
-	DeliveryCityID int NOT NULL,
-	PostalCityID int NOT NULL,
-	PhoneNumber nvarchar(20) NOT NULL,	
-	FaxNumber nvarchar(20) NOT NULL,
-	WebsiteURL nvarchar(256) NOT NULL,
-	DeliveryAddressLine1 nvarchar(60) NOT NULL,	
-	PostalPostalCode nvarchar(10) NOT NULL,
+CREATE TABLE StagingSupplierCategories
+(
+	SupplierCategoryID INT NOT NULL,
+	SupplierCategoryName NVARCHAR(100) NOT NULL
+)
+
+
+IF OBJECT_ID('dbo.StagingSupplier', 'U') IS NOT NULL
+    DROP TABLE StagingSupplier
+CREATE TABLE StagingSupplier
+(
+	SupplierID INT NOT NULL PRIMARY KEY,
+	SupplierName NVARCHAR(100) NOT NULL,
+
+	SupplierCategoryID INT NOT NULL,
+	SupplierCategoryName NVARCHAR(100) NOT NULL,
+	--^ from Supplier Categories
+
+	PrimaryContactPersonID INT NOT NULL,
+	AlternateContactPersonID INT NOT NULL,
+
+	DeliveryMethodID INT ,
+	DeliveryCityID INT NOT NULL,
+	PostalCityID INT NOT NULL,
+	SupplierReference NVARCHAR(50) NULL,
+
+	BankAccountName NVARCHAR(100) NOT NULL,
+	BankAccountBranch NVARCHAR(100) NOT NULL,
+	BankAccountCode NVARCHAR(100) NOT NULL,
+	BankAccountNumber NVARCHAR(100) NOT NULL,
+	BankInternationalCode NVARCHAR(100) NOT NULL,
+
+	PhoneNumber NVARCHAR(20) NOT NULL,
+	FaxNumber NVARCHAR(20) NOT NULL,
+	WebsiteURL NVARCHAR(256) NOT NULL,
+
+	DeliveryAddressLine1 NVARCHAR(60) NOT NULL,
+	DeliveryAddressLine2 NVARCHAR(60) NOT NULL,
+	DeliveryPostalCode NVARCHAR(20) NOT NULL,
+
+	PostalAddressLine1 NVARCHAR(60) NOT NULL,
+	PostalAddressLine2 NVARCHAR(60) NOT NULL,
+	PostalPostalCode NVARCHAR(20) NOT NULL
 );
 GO
 
-
-IF OBJECT_ID('dbo.StagingOrderline', 'U') IS NOT NULL
-    drop table StagingOrderline
-CREATE TABLE StagingOrderline(
-	PurchaseOrderLineID int NOT NULL PRIMARY KEY,
-	PurchaseOrderID int NOT NULL,
-	StockItemID int NOT NULL,
-	OrderedOuters int NOT NULL,
-	Description nvarchar(100) NOT NULL,
-	ReceivedOuters int NOT NULL,
-	PackageTypeID int NOT NULL,
-	ExpectedUnitPricePerOuter [decimal](18, 2) NULL,
-	LastReceiptDate date NULL,
-	IsOrderLineFinalized bit NOT NULL,
-	LastEditedBy int NOT NULL,
-	LastEditedWhen datetime2(7) NOT NULL,
-);
+IF OBJECT_ID('dbo.StagingSupplierTransactions', 'U') IS NOT NULL
+    DROP TABLE StagingSupplierTransactions
+CREATE TABLE StagingSupplierTransactions
+(
+	[SupplierTransactionID] [int] NOT NULL,
+	[SupplierID] [int] NOT NULL,
+	[TransactionTypeID] [int] NOT NULL,
+	[PurchaseOrderID] [int] NULL,
+	[PaymentMethodID] [int] NULL,
+	[SupplierInvoiceNumber] [nvarchar](20) NULL,
+	[TransactionDate] [date] NOT NULL,
+	[AmountExcludingTax] [decimal](20, 2) NOT NULL,
+	[TaxAmount] [decimal](20, 2) NOT NULL,
+	[TransactionAmount] [decimal](20, 2) NOT NULL,
+	[OutstandingBalance] [decimal](20, 2) NOT NULL,
+	[FinalizationDate] [date] NULL,
+	[IsFinalized] NVARCHAR(10) NOT NULL,
+)
 GO
---
+++ Source/Staging/Purchase/StagingETL.sql
@@ -0,0 +1,232 @@
+CREATE OR ALTER PROCEDURE FillStagingPurchaseOrder
+AS
+BEGIN
+    TRUNCATE TABLE StagingPurchaseOrder
+    INSERT INTO StagingPurchaseOrder
+        (
+        PurchaseOrderID,
+        SupplierID,
+        OrderDate,
+        DeliveryMethodID,
+        ContactPersonID,
+        ExpectedDeliveryDate,
+        SupplierReference,
+        IsOrderFinalized
+        )
+    SELECT
+        PurchaseOrderID,
+        SupplierID,
+        OrderDate,
+        DeliveryMethodID,
+        ContactPersonID,
+        ExpectedDeliveryDate,
+        SupplierReference,
+        CASE IsOrderFinalized
+		    WHEN 1 then 'Yes' else 'No' 
+	    END
+
+    FROM [WideWorldImporters].[Purchasing].[PurchaseOrders]
+
+    -------------------------------------------------------
+
+    DECLARE @AFFECTED_ROWS INT;
+    SET @AFFECTED_ROWS = @@ROWCOUNT;
+
+    DECLARE @CURRENT_INSERT_DATETIME DATETIME2 = (select CONVERT(DATETIME2, GETDATE()));
+
+    exec [AddStagingLog] 
+    @procedure_name = 'FillStagingPurchaseOrder',
+    @action = 'INSERT',
+    @TargetTable = 'StagingPurchaseOrder',
+    @Datetime = @CURRENT_INSERT_DATETIME,
+    @AffectedRowsNumber = @AFFECTED_ROWS
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+
+
+CREATE OR ALTER PROCEDURE FillStagingSupplierCategories
+AS
+BEGIN
+    TRUNCATE TABLE StagingSupplierCategories
+    INSERT INTO StagingSupplierCategories
+        (
+        SupplierCategoryID,
+        SupplierCategoryName
+        )
+    SELECT
+        SupplierCategoryID,
+        SupplierCategoryName
+
+    FROM [WideWorldImporters].[Purchasing].[SupplierCategories]
+
+    -------------------------------------------------------
+
+    DECLARE @AFFECTED_ROWS INT;
+    SET @AFFECTED_ROWS = @@ROWCOUNT;
+
+    DECLARE @CURRENT_INSERT_DATETIME DATETIME2 = (select CONVERT(DATETIME2, GETDATE()));
+
+    exec [AddStagingLog] 
+    @procedure_name = 'FillStagingSupplierCategories',
+    @action = 'INSERT',
+    @TargetTable = 'StagingSupplierCategories',
+    @Datetime = @CURRENT_INSERT_DATETIME,
+    @AffectedRowsNumber = @AFFECTED_ROWS
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+
+CREATE OR ALTER PROCEDURE FillStagingSupplier
+AS
+BEGIN
+    TRUNCATE TABLE StagingSupplier
+    INSERT INTO StagingSupplier
+        (
+        SupplierID,
+        SupplierName,
+        SupplierCategoryID,
+        SupplierCategoryName,
+        PrimaryContactPersonID,
+        AlternateContactPersonID,
+        DeliveryMethodID,
+        DeliveryCityID,
+        PostalCityID,
+        SupplierReference,
+        BankAccountName,
+        BankAccountBranch,
+        BankAccountCode,
+        BankAccountNumber,
+        BankInternationalCode,
+        PhoneNumber,
+        FaxNumber,
+        WebsiteURL,
+        DeliveryAddressLine1,
+        DeliveryAddressLine2,
+        DeliveryPostalCode,
+        PostalAddressLine1,
+        PostalAddressLine2,
+        PostalPostalCode
+        )
+    SELECT
+        SupplierID,
+        SupplierName,
+        a.SupplierCategoryID,
+        b.SupplierCategoryName,
+        PrimaryContactPersonID,
+        AlternateContactPersonID,
+        DeliveryMethodID,
+        DeliveryCityID,
+        PostalCityID,
+        SupplierReference,
+        BankAccountName,
+        BankAccountBranch,
+        BankAccountCode,
+        BankAccountNumber,
+        BankInternationalCode,
+        PhoneNumber,
+        FaxNumber,
+        WebsiteURL,
+        DeliveryAddressLine1,
+        DeliveryAddressLine2,
+        DeliveryPostalCode,
+        PostalAddressLine1,
+        PostalAddressLine2,
+        PostalPostalCode
+
+    FROM [WideWorldImporters].[Purchasing].[Suppliers] a INNER JOIN [WideWorldImporters].[Purchasing].[SupplierCategories] b
+    ON (a.SupplierCategoryID = b.SupplierCategoryID)
+
+    -------------------------------------------------------
+
+    DECLARE @AFFECTED_ROWS INT;
+    SET @AFFECTED_ROWS = @@ROWCOUNT;
+
+    DECLARE @CURRENT_INSERT_DATETIME DATETIME2 = (select CONVERT(DATETIME2, GETDATE()));
+
+    exec [AddStagingLog] 
+    @procedure_name = 'FillStagingSupplier',
+    @action = 'INSERT',
+    @TargetTable = 'StagingSupplier',
+    @Datetime = @CURRENT_INSERT_DATETIME,
+    @AffectedRowsNumber = @AFFECTED_ROWS
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+
+CREATE OR ALTER PROCEDURE FillStagingSupplierTransactions
+AS
+BEGIN
+    TRUNCATE TABLE StagingSupplierTransactions
+    INSERT INTO StagingSupplierTransactions
+        (
+        SupplierTransactionID,
+        SupplierID,
+        TransactionTypeID,
+        PurchaseOrderID,
+        PaymentMethodID,
+        SupplierInvoiceNumber,
+        TransactionDate,
+        AmountExcludingTax,
+        TaxAmount,
+        TransactionAmount,
+        OutstandingBalance,
+        FinalizationDate,
+        IsFinalized
+        )
+    SELECT
+        SupplierTransactionID,
+        SupplierID,
+        TransactionTypeID,
+        PurchaseOrderID,
+        PaymentMethodID,
+        SupplierInvoiceNumber,
+        TransactionDate,
+        AmountExcludingTax,
+        TaxAmount,
+        TransactionAmount,
+        OutstandingBalance,
+        FinalizationDate,
+        CASE IsFinalized
+		    WHEN 1 then 'Yes' else 'No' 
+	    END
+
+    FROM [WideWorldImporters].[Purchasing].[SupplierTransactions]
+
+    -------------------------------------------------------
+
+    DECLARE @AFFECTED_ROWS INT;
+    SET @AFFECTED_ROWS = @@ROWCOUNT;
+
+    DECLARE @CURRENT_INSERT_DATETIME DATETIME2 = (select CONVERT(DATETIME2, GETDATE()));
+
+    exec [AddStagingLog] 
+    @procedure_name = 'FillStagingSupplierTransactions',
+    @action = 'INSERT',
+    @TargetTable = 'StagingSupplierTransactions',
+    @Datetime = @CURRENT_INSERT_DATETIME,
+    @AffectedRowsNumber = @AFFECTED_ROWS
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
+
+
+CREATE OR ALTER PROCEDURE FILL_PURCHASING_STAGING_AREA
+AS
+BEGIN
+
+    EXEC FillStagingPurchaseOrder;
+    EXEC FillStagingSupplierCategories;
+    EXEC FillStagingSupplier;
+    EXEC FillStagingSupplierTransactions;
+
+END
+--------------------------------------------------------------------------------------------------------------
+GO
